f1b0c57def54c459d52b3b1aa1f80905b9f88826
takanotume24 <takanotume24.personal@takanotume24.sakura.ne.jp>
2026-07-04 21:26:43 +0900

    VM Importのpositional_record_conをレコード構文で0件化
    - CompileImportStmt.hs: Instruction/Value/Stmt/Exprのpositional constructorを全てレコード構文に変更
    - 行数増加を200行以下に抑えるため、Import/FromImportの各ケースをコンパクトに保つ

--- src/PythonHS/VM/CompileImportStmt.hs
+++ src/PythonHS/VM/CompileImportStmt.hs
@@ -1,25 +1,31 @@
 module PythonHS.VM.CompileImportStmt (compileImportStmt) where
 
-import PythonHS.AST.Stmt (Stmt (FromImportStmt, ImportStmt))
+import PythonHS.AST.Stmt (Stmt (..))
 import PythonHS.Evaluator.ShowPos (showPos)
-import PythonHS.Evaluator.Value (Value (ModuleValue, StringValue))
+import PythonHS.Evaluator.Value (Value (..))
 import PythonHS.VM.CompileExprResult (CompileExprResult (..))
-import PythonHS.VM.Instruction (Instruction (CallFunction, DefineFunction, LoadName, PushConst, ReturnTop, StoreName))
+import PythonHS.VM.Instruction (Instruction (..))
 
 compileImportStmt :: Int -> Stmt -> Either String CompileExprResult
 compileImportStmt importBaseIndex stmt =
   case stmt of
-    ImportStmt modules pos -> compileImportModules importBaseIndex modules pos
-    FromImportStmt relativeLevel modulePath importedNames pos -> compileFromImport importBaseIndex relativeLevel modulePath importedNames pos
+    ImportStmt {importStmtItems = modules, importStmtPos = pos} -> compileImportModules importBaseIndex modules pos
+    FromImportStmt {fromImportStmtLevel = relativeLevel, fromImportStmtModule = modulePath, fromImportStmtItems = importedNames, fromImportStmtPos = pos} ->
+      compileFromImport importBaseIndex relativeLevel modulePath importedNames pos
     _ -> Left "VM compile error: compileImportStmt called with non-import statement"
   where
     compileImportModules baseIndex modules pos =
       case modules of
-        [] -> Right (CompileExprResult [] baseIndex)
+        [] -> Right (CompileExprResult {compileExprResultCode = [], compileExprResultEndIndex = baseIndex})
         (modulePath, maybeAlias) : rest -> do
           firstResult <- compileSingleImport baseIndex modulePath maybeAlias pos
           restResult <- compileImportModules (compileExprResultEndIndex firstResult) rest pos
-          pure (CompileExprResult (compileExprResultCode firstResult ++ compileExprResultCode restResult) (compileExprResultEndIndex restResult))
+          pure
+            ( CompileExprResult
+                { compileExprResultCode = compileExprResultCode firstResult ++ compileExprResultCode restResult,
+                  compileExprResultEndIndex = compileExprResultEndIndex restResult
+                }
+            )
 
     compileSingleImport baseIndex modulePath maybeAlias pos =
       if null modulePath
@@ -27,17 +33,17 @@ compileImportStmt importBaseIndex stmt =
         else case maybeAlias of
           Just aliasName ->
             let moduleName = joinModulePath modulePath
-                importCode = [PushConst (ModuleValue moduleName []), StoreName aliasName]
-             in Right (CompileExprResult importCode (baseIndex + length importCode))
+                importCode = [PushConst {pushConstValue = ModuleValue {moduleValueName = moduleName, moduleValueAttrs = []}}, StoreName {storeNameName = aliasName}]
+             in Right (CompileExprResult {compileExprResultCode = importCode, compileExprResultEndIndex = baseIndex + length importCode})
           Nothing ->
             case modulePath of
               [singleName] ->
-                let importCode = [PushConst (ModuleValue singleName []), StoreName singleName]
-                 in Right (CompileExprResult importCode (baseIndex + length importCode))
+                let importCode = [PushConst {pushConstValue = ModuleValue {moduleValueName = singleName, moduleValueAttrs = []}}, StoreName {storeNameName = singleName}]
+                 in Right (CompileExprResult {compileExprResultCode = importCode, compileExprResultEndIndex = baseIndex + length importCode})
               rootName : _ ->
                 let rootValue = buildRootModuleValue modulePath
-                    importCode = [PushConst rootValue, StoreName rootName]
-                 in Right (CompileExprResult importCode (baseIndex + length importCode))
+                    importCode = [PushConst {pushConstValue = rootValue}, StoreName {storeNameName = rootName}]
+                 in Right (CompileExprResult {compileExprResultCode = importCode, compileExprResultEndIndex = baseIndex + length importCode})
               [] -> Left ("Import error: unsupported module  at " ++ showPos pos)
 
     compileFromImport baseIndex relativeLevel modulePath importedNames pos
@@ -47,9 +53,14 @@ compileImportStmt importBaseIndex stmt =
           Left ("Import error: expected imported name at " ++ showPos pos)
       | modulePath == ["math"] = do
           let moduleAlias = "__python_hs_import_math"
-              setupCode = [PushConst (StringValue "<module:math>"), StoreName moduleAlias]
+              setupCode = [PushConst {pushConstValue = StringValue {stringValue = "<module:math>"}}, StoreName {storeNameName = moduleAlias}]
           importedResult <- compileFromMathItems (baseIndex + 2) moduleAlias importedNames pos
-          pure (CompileExprResult (setupCode ++ compileExprResultCode importedResult) (compileExprResultEndIndex importedResult))
+          pure
+            ( CompileExprResult
+                { compileExprResultCode = setupCode ++ compileExprResultCode importedResult,
+                  compileExprResultEndIndex = compileExprResultEndIndex importedResult
+                }
+            )
       | modulePath == ["dataclasses"] =
           compileFromDataclassesItems baseIndex importedNames pos
       | otherwise =
@@ -57,11 +68,16 @@ compileImportStmt importBaseIndex stmt =
 
     compileFromMathItems baseIndex moduleAlias importedNames pos =
       case importedNames of
-        [] -> Right (CompileExprResult [] baseIndex)
+        [] -> Right (CompileExprResult {compileExprResultCode = [], compileExprResultEndIndex = baseIndex})
         (name, maybeAlias) : rest -> do
           firstResult <- compileFromMathItem baseIndex moduleAlias name maybeAlias pos
           restResult <- compileFromMathItems (compileExprResultEndIndex firstResult) moduleAlias rest pos
-          pure (CompileExprResult (compileExprResultCode firstResult ++ compileExprResultCode restResult) (compileExprResultEndIndex restResult))
+          pure
+            ( CompileExprResult
+                { compileExprResultCode = compileExprResultCode firstResult ++ compileExprResultCode restResult,
+                  compileExprResultEndIndex = compileExprResultEndIndex restResult
+                }
+            )
 
     compileFromMathItem baseIndex moduleAlias name maybeAlias pos =
       let targetName =
@@ -70,15 +86,25 @@ compileImportStmt importBaseIndex stmt =
               Nothing -> name
        in if name == "pi" || name == "e"
             then
-              let callArgs = [([LoadName moduleAlias pos], Nothing, pos)]
-               in Right (CompileExprResult [CallFunction name callArgs pos, StoreName targetName] (baseIndex + 2))
+              let callArgs = [([LoadName {loadNameName = moduleAlias, loadNamePos = pos}], Nothing, pos)]
+               in Right
+                    ( CompileExprResult
+                        { compileExprResultCode = [CallFunction {callFunctionName = name, callFunctionArgs = callArgs, callFunctionPos = pos}, StoreName {storeNameName = targetName}],
+                          compileExprResultEndIndex = baseIndex + 2
+                        }
+                    )
             else
               if isMathUnaryFunction name
                 then
                   let wrapperParam = "__python_hs_import_arg"
-                      callArgs = [([LoadName moduleAlias pos], Nothing, pos), ([LoadName wrapperParam pos], Nothing, pos)]
-                      wrapperBody = [CallFunction name callArgs pos, ReturnTop]
-                   in Right (CompileExprResult [DefineFunction targetName [wrapperParam] [] wrapperBody] (baseIndex + 1))
+                      callArgs = [([LoadName {loadNameName = moduleAlias, loadNamePos = pos}], Nothing, pos), ([LoadName {loadNameName = wrapperParam, loadNamePos = pos}], Nothing, pos)]
+                      wrapperBody = [CallFunction {callFunctionName = name, callFunctionArgs = callArgs, callFunctionPos = pos}, ReturnTop]
+                   in Right
+                        ( CompileExprResult
+                            { compileExprResultCode = [DefineFunction {defineFunctionName = targetName, defineFunctionParams = [wrapperParam], defineFunctionDefaultCodes = [], defineFunctionCode = wrapperBody}],
+                              compileExprResultEndIndex = baseIndex + 1
+                            }
+                        )
                 else Left ("Import error: unsupported module member " ++ name ++ " at " ++ showPos pos)
 
     isMathUnaryFunction name =
@@ -91,7 +117,7 @@ compileImportStmt importBaseIndex stmt =
 
     compileFromDataclassesItems baseIndex importedNames pos =
       case importedNames of
-        [] -> Right (CompileExprResult [] baseIndex)
+        [] -> Right (CompileExprResult {compileExprResultCode = [], compileExprResultEndIndex = baseIndex})
         (name, maybeAlias) : rest ->
           if name == "dataclass" || name == "field"
             then do
@@ -99,9 +125,14 @@ compileImportStmt importBaseIndex stmt =
                     case maybeAlias of
                       Just aliasName -> aliasName
                       Nothing -> name
-                  firstCode = [PushConst (StringValue ("<dataclasses:" ++ name ++ ">")), StoreName targetName]
+                  firstCode = [PushConst {pushConstValue = StringValue {stringValue = "<dataclasses:" ++ name ++ ">"}}, StoreName {storeNameName = targetName}]
               restResult <- compileFromDataclassesItems (baseIndex + 2) rest pos
-              pure (CompileExprResult (firstCode ++ compileExprResultCode restResult) (compileExprResultEndIndex restResult))
+              pure
+                ( CompileExprResult
+                    { compileExprResultCode = firstCode ++ compileExprResultCode restResult,
+                      compileExprResultEndIndex = compileExprResultEndIndex restResult
+                    }
+                )
             else Left ("Import error: unsupported module member " ++ name ++ " at " ++ showPos pos)
 
     joinModulePath segments =
@@ -112,13 +143,13 @@ compileImportStmt importBaseIndex stmt =
 
     buildRootModuleValue segments =
       case segments of
-        [] -> ModuleValue "" []
+        [] -> ModuleValue {moduleValueName = "", moduleValueAttrs = []}
         root : rest ->
-          ModuleValue root (buildAttrs root rest)
+          ModuleValue {moduleValueName = root, moduleValueAttrs = buildAttrs root rest}
 
     buildAttrs _ [] = []
     buildAttrs prefix [leaf] =
-      [(leaf, ModuleValue (prefix ++ "." ++ leaf) [])]
+      [(leaf, ModuleValue {moduleValueName = prefix ++ "." ++ leaf, moduleValueAttrs = []})]
     buildAttrs prefix (x : xs) =
       let nextPrefix = prefix ++ "." ++ x
-       in [(x, ModuleValue nextPrefix (buildAttrs nextPrefix xs))]
+       in [(x, ModuleValue {moduleValueName = nextPrefix, moduleValueAttrs = buildAttrs nextPrefix xs})]
